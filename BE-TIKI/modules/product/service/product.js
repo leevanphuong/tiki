39207497ce35d058f0ba9c0e4cc84aa8abc6efe4
anhtvtph19306 <118867306+anhtvtph19306@users.noreply.github.com>
2023-06-25 17:05:16 +0700

    feat: create-remove-product-api-product tôi đã làm xong phần removeProduct refs: DTNF-6

--- BE-TIKI/modules/product/service/product.js
+++ BE-TIKI/modules/product/service/product.js
@@ -1,6 +1,11 @@
 import product from "../model/product.js"
 
-export const getAllproduct= async()=>{
-    const products= await product.find()
+export const getAllproduct = async () => {
+    const products = await product.find()
     return products
+}
+
+export const removeProduct = async (req, res) => {
+    const deleteProduct = await product.findByIdAndDelete(req.params.id)
+    return deleteProduct
 }
--- BE-TIKI/routers/product.js
+++ BE-TIKI/routers/product.js
@@ -2,4 +2,5 @@ import express from 'express'
 import productController from '../modules/product/controller/index.js'
 const router = express.Router()
 router.get("/", productController.getAllproducts)
+router.delete("/:id", productController.deleteProduct)
 export default router 
